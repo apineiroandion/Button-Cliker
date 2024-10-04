edb0e431154dae227f82ad91ee6325fc688eb896
apineiroandion <apineiroandion@danielcastelao.org>
2024-10-04 13:08:25 +0200

    implemento grid layout y mecanica de completaciones

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QProgressBar
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QProgressBar, QGridLayout
 
 app = QApplication([])
 
 window = QWidget()
-layout = QVBoxLayout()
+layout = QGridLayout()
 
 window.setWindowTitle('Button Clicker')
 window.setGeometry(100, 100, 400, 300)
@@ -17,12 +17,17 @@ button3 = QPushButton('Botón 3')
 x = 0
 y = 0
 z = 0
+completaciones = 0
 
 def actualizar(param):
     elegir_variable(param)
     actualizar_perCent()
     progressBar.setValue(perCent)
-    completar()
+    if completar() == True:
+        global completaciones
+        completaciones += 1
+        panel0F.setText(string_completaciones())
+
 
 def elegir_variable(param):
     if param == 1:
@@ -50,6 +55,9 @@ def string_y():
 def string_z():
     return str(z)
 
+def string_completaciones():
+    return str(completaciones)
+
 """Progress bar"""
 perCent = 0
 def actualizar_perCent():
@@ -66,6 +74,8 @@ def completar():
         print("Bien!!!! Has tocado 100 Botones!!!")
         perCent = 0
         progressBar.setValue(perCent)
+        return True
+
 
 
 button1.clicked.connect(lambda: actualizar(1))
@@ -73,28 +83,34 @@ button2.clicked.connect(lambda: actualizar(2))
 button3.clicked.connect(lambda: actualizar(3))
 
 """Paneles de texto"""
+panel0 = QLabel('Completaciones')
 panel1 = QLabel('Panel 1')
 panel2 = QLabel('Panel 2')
 panel3 = QLabel('Panel 3')
+panel0F = QLabel(string_completaciones())
 panel1F = QLabel(string_x())
 panel2F = QLabel(string_y())
 panel3F = QLabel(string_z())
 
-layout.addWidget(panel1)
-layout.addWidget(button1)
-layout.addWidget(panel1F)
-layout.addWidget(panel2)
-layout.addWidget(button2)
-layout.addWidget(panel2F)
-layout.addWidget(panel3)
-layout.addWidget(button3)
-layout.addWidget(panel3F)
-layout.addWidget(progressBar)
-layout.addStretch()
+layout.addWidget(panel0, 0, 0, 1, 3)
+layout.addWidget(panel0F, 0, 1, 1, 2)
+layout.addWidget(panel1, 1, 0)
+layout.addWidget(button1, 1, 1)
+layout.addWidget(panel1F, 1, 2)
+layout.addWidget(panel2, 2, 0)
+layout.addWidget(button2, 2, 1)
+layout.addWidget(panel2F, 2, 2)
+layout.addWidget(panel3, 3, 0)
+layout.addWidget(button3, 3, 1)
+layout.addWidget(panel3F, 3, 2)
+layout.addWidget(progressBar, 4, 0, 1, 3)
+"""layout.addStretch()"""
 
+layout.setAlignment(panel0, Qt.AlignCenter)
 layout.setAlignment(panel1, Qt.AlignCenter)
 layout.setAlignment(panel2, Qt.AlignCenter)
 layout.setAlignment(panel3, Qt.AlignCenter)
+layout.setAlignment(panel0F, Qt.AlignCenter)
 layout.setAlignment(panel1F, Qt.AlignCenter)
 layout.setAlignment(panel2F, Qt.AlignCenter)
 layout.setAlignment(panel3F, Qt.AlignCenter)
